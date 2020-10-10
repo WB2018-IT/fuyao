d06becc7c73836a0ad20e67172bde4a34d1526f9
wangb <1871331217@qq.com>
2020-10-10 17:34:40 +0800

    add:添加授权中心服务

---
+++ fuyao-auth/fuyao-auth-common/src/main/java/com/fuyao/auth/entity/JwtConstans.java
@@ -0,0 +1,6 @@
+package com.fuyao.auth.entity;
+
+public abstract class JwtConstans {
+    public static final String JWT_KEY_ID = "id";
+    public static final String JWT_KEY_USER_NAME = "username";
+}
