c904fe251b95cc6cd87a04017ec663c6c4b57f6b
wangb <1871331217@qq.com>
2020-10-19 10:39:09 +0800

    update:1

--- fuyao-auth/fuyao-auth-service/src/main/java/com/fuyao/auth/controller/AuthController.java
+++ fuyao-auth/fuyao-auth-service/src/main/java/com/fuyao/auth/controller/AuthController.java
@@ -2,13 +2,49 @@ package com.fuyao.auth.controller;
 
 import com.fuyao.auth.config.JwtProperties;
 import com.fuyao.auth.service.AuthService;
+import com.fuyao.common.utils.CookieUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 @EnableConfigurationProperties(JwtProperties.class)
 public class AuthController {
     @Autowired
     private AuthService authService;
+
+    @Autowired
+    private JwtProperties jwtProperties;
+
+    /**
+     * 登录授权
+     *
+     * @param username
+     * @param password
+     * @return
+     */
+    @PostMapping("accredit")
+    public ResponseEntity<Void> authentication(
+            @RequestParam("username") String username,
+            @RequestParam("password") String password,
+            HttpServletRequest request,
+            HttpServletResponse response
+    ){
+        // 登录校验
+        String token = this.authService.authentication(username, password);
+        if(StringUtils.isBlank(token)){
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+        // 将token写入cookie,并指定httpOnly为true，防止通过JS获取和修改
+        CookieUtils.setCookie(request,response,jwtProperties.getCookieName(),token,jwtProperties.getCookieMaxAge(),null,true);
+        return ResponseEntity.ok().build();
+    }
 }
